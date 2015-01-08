0ab784c26309045af59cab17dd08fce0fafdedb8
MpEliaz <eliasmp77@gmail.com>
2015-01-08 16:13:05 -0300

    avance

--- chiloeplaces/api/models.py
+++ chiloeplaces/api/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Lugares(models.Model):
+class Lugar(models.Model):
     nombre = models.CharField(max_length=140)
     direccion = models.CharField(max_length=200)
     telefono = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=300)
-    ubicacion = models.ForeignKey('Ubicacion')
-    creado_por = models.ForeignKey('User')
-    fecha = models.DateTimeField()
+    creado_por = models.ForeignKey(User)
+    fecha = models.DateTimeField(auto_now=True)
 
-class ubicacion(models.Model):
+class Ubicacion(models.Model):
     latitud = models.FloatField()
     longitud = models.FloatField()
+    lugar = models.ForeignKey('Lugar')
 
 
