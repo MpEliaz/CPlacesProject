bbed986a477625175d270fa693f4be24b6813738
MpEliaz <eliasmp77@gmail.com>
2015-01-12 16:59:38 -0300

    coreccion relacion many2many

--- chiloeplaces/api/models.py
+++ chiloeplaces/api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Imagenes(models.Model):
+    url = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=300, blank=True)
+    #lugar = models.ForeignKey('Lugar', related_name='imagenes', null=True)
+
+    def __unicode__(self):
+		return u'{0}'.format(self.url)
+
 class Lugar(models.Model):
     nombre = models.CharField(max_length=140)
     direccion = models.CharField(max_length=200)
@@ -10,6 +18,7 @@ class Lugar(models.Model):
     creado_por = models.ForeignKey(User)
     fecha = models.DateTimeField(auto_now=True)
     estado = models.BooleanField(default=False)
+    imagenes = models.ManyToManyField(Imagenes, null=True, blank=True)
 
     def __unicode__(self):
         return u'{0}'.format(self.nombre)
@@ -32,25 +41,16 @@ class Comuna(models.Model):
 	def __unicode__(self):
 		return u'{0}'.format(self.nombre)
 
-class Imagenes(models.Model):
-    url = models.CharField(max_length=200)
-    descripcion = models.CharField(max_length=300, blank=True)
-    lugar = models.ForeignKey('Lugar', related_name='imagenes', null=True)
-
-    def __unicode__(self):
-		return u'{0}'.format(self.url)
-
-
 class Actividad(models.Model):
     nombre = models.CharField(max_length=140)
-    direccion = models.CharField(max_length=140)
+    direccion = models.CharField(max_length=140, null=True)
     descripcion = models.CharField(max_length=300)
-    valor = models.IntegerField(blank=True)
+    valor = models.IntegerField(blank=True, null=True)
     fecha_inicio = models.DateTimeField(blank=True)
-    fecha_termino = models.DateTimeField(blank=True)
+    fecha_termino = models.DateTimeField(blank=True, null=True)
     lugar = models.CharField(max_length=140)
     comuna = models.OneToOneField(Comuna)
-
+    imagenes = models.ManyToManyField(Imagenes, null=True, blank=True)
     def __unicode__(self):
         return u'{0}'.format(self.nombre)
 
